8154d482de731d6e26b371d4c50d4e9d12780160
Raul Barison <raulgbarison@gmail.com>
2025-10-18 20:46:37 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+// Portal RB — script mínimo
+document.addEventListener('DOMContentLoaded', () => {
+  const btn = document.getElementById('portalRadioBtn');
+  if(btn){
+    btn.addEventListener('click', () => {
+      alert('??? Rádio em breve. Em breve você poderá inserir a URL de streaming nas configurações do site.');
+    });
+  }
+});
